c7d54bd9f874c2e7fd553c0367fd21d160ef08b2
ssssota <tommysouta@gmail.com>
2022-03-25 22:53:57 +0900

    Adjust font-size

--- src/components/PaddingPane.svelte
+++ src/components/PaddingPane.svelte
@@ -4,24 +4,15 @@
 
 <style>
   div {
-    --vertical-padding: 1.6rem;
-    --horizontal-padding: 3rem;
+    --vertical-padding: calc(var(--slide-height) * 0.075);
+    --horizontal-padding: calc(var(--slide-width) * 0.075);
     padding: var(--vertical-padding) var(--horizontal-padding);
     height: 100%;
     position: relative;
     box-sizing: border-box;
   }
-  :global(h1) {
-    margin: 2rem 0 0 0;
-  }
-  :global(h2) {
-    margin: 1.5rem 0 0 0;
-  }
-  :global(h3) {
-    margin: 1.2rem 0 0 0;
-  }
-  :global(h4, h5, h6, p) {
-    margin: 1rem 0 0 0;
+  :global(h1, h2, h3, h4, h5, h6, p) {
+    margin: 1em 0 0 0;
   }
 
   @media print {
--- src/components/SlideView.svelte
+++ src/components/SlideView.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-  import { page } from "../utils/page";
-
   export let onLeftClick: svelte.JSX.EventHandler<MouseEvent, HTMLDivElement> | undefined = undefined;
   export let onRightClick: svelte.JSX.EventHandler<MouseEvent, HTMLDivElement> | undefined = undefined;
-  export let pageCount: boolean | undefined = undefined;
 </script>
 
 <div class="wrapper">
   <section class="slide">
     <slot />
   </section>
-  <div class:cursor={onLeftClick != null} class="left" on:click={onLeftClick} />
-  <div class:cursor={onRightClick != null} class="right" on:click={onRightClick} />
-  {#if pageCount === true}
-    <div class="page-count">{$page}</div>
+  {#if onLeftClick != null}
+    <div class:cursor={onLeftClick != null} class="left" on:click={onLeftClick} />
+  {/if}
+  {#if onRightClick != null}
+    <div class:cursor={onRightClick != null} class="right" on:click={onRightClick} />
   {/if}
 </div>
 
@@ -40,7 +38,7 @@
     top: 0;
   }
   .left.cursor {
-    cursor: url(../images/left.svg) 0 0, pointer;
+    cursor: url(./images/left.svg) 0 0, pointer;
   }
   .right {
     position: absolute;
@@ -50,47 +48,30 @@
     top: 0;
   }
   .right.cursor {
-    cursor: url(../images/right.svg) 0 0, pointer;
+    cursor: url(./images/right.svg) 0 0, pointer;
   }
-  .page-count {
-    --size: 1.5em;
-    position: fixed;
-    height: var(--size);
-    width: var(--size);
-    border-radius: 50%;
-    background-color: antiquewhite;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 0 calc(var(--size) / 2) 0 rgba(0, 0, 0, 0.5);
-
-    bottom: calc(var(--size) / 2);
-    right: calc(var(--size) / 2);
-  }
-
-  :global(html) {
+  :root {
     --slide-width: min(100vw, calc(400vh / 3));
     --slide-height: min(75vw, 100vh);
-
-    font-size: calc(var(--slide-width) / 40);
+    --base-font-size: calc(var(--slide-width) / 40);
   }
 
   .slide {
-    background-color: white;
+    background-color: var(--background-color, white);
 
     width: var(--slide-width);
     height: var(--slide-height);
   }
   .slide :global(h1) {
-    font-size: 2rem;
+    font-size: calc(var(--base-font-size) * 2);
   }
   .slide :global(h2) {
-    font-size: 1.6rem;
+    font-size: calc(var(--base-font-size) * 1.6);
   }
   .slide :global(h3) {
-    font-size: 1.4rem;
+    font-size: calc(var(--base-font-size) * 1.4);
   }
   .slide :global(h4), .slide :global(h5), .slide :global(h6), .slide :global(p), .slide :global(li) {
-    font-size: 1.2rem;
+    font-size: calc(var(--base-font-size) * 1.2);
   }
 </style>
